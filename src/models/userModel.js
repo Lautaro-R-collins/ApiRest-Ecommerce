bb0393cd6572ed8ecccade566b1a588765f0b767
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2025-11-26 16:20:31 -0300

    auth model

---
+++ src/models/userModel.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minLength: 6,
+        maxLength: 255,
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6,
+        maxLength: 254,
+    },
+    username: {
+        type: String,
+        default: '',
+        required: true,
+        trim: true,
+        minLength: 3,
+        maxLength: 20,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+        required: true,
+    }
+})
+
+export default mongoose.model('User', userSchema)
