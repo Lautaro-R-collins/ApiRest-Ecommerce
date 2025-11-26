f965e31bbdb4b0c2b4a284b321dad0c1cb60616f
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2025-11-26 16:20:11 -0300

    add routes in server

--- src/server.js
+++ src/server.js
@@ -1,9 +1,27 @@
-import express from "express";
+import express from 'express'
+import { config } from 'dotenv'
+import { connectDB, disconnectDB } from './config/configDB.js'
 
-const app = express();
+//import routes
+import authRoutes from './routes/authRoutes.js'
 
-const PORT = process.env.PORT || 3000;
+config()
 
-app.listen(PORT, () => {
-    console.log(`servidor corriendo en el puerto ${PORT}`)
-})
+const app = express()
+const PORT = process.env.PORT || 3000
+
+// Rutas Api
+
+app.use('/api/auth', authRoutes)
+
+connectDB()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Servidor corriendo en el puerto ${PORT}`)
+        })
+    })
+    .catch(() => {
+        disconnectDB()
+    })
+
+    
