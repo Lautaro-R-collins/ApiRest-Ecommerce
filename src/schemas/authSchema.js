09051f4f6178c294693151ec7cd3e052456e028f
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2025-11-27 03:39:43 -0300

    add - auth schema

---
+++ src/schemas/authSchema.js
@@ -0,0 +1,7 @@
+import { z } from 'zod';
+
+export const registerSchema = z.object({
+    username: z.string().min(3).max(20),
+    password: z.string().min(6).max(254),
+    email: z.email().min(6).max(254),
+});
