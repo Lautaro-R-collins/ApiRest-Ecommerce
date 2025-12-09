a852b86b538d5ef649adb05fd220c4f4ff61b570
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2025-12-09 18:42:57 -0300

    review routes

--- src/routes/reviewRoutes.js
+++ src/routes/reviewRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createReview, getReviewsByProduct, deleteReview } from "../controllers/reviewController.js";
+import { createReview, getReviewsByProduct } from "../controllers/reviewController.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
@@ -9,10 +9,5 @@ router.get("/:productId", getReviewsByProduct);
 
 // POST reseña (requiere login)
 router.post("/:productId", authMiddleware, createReview);
-
-// DELETE reseña (requiere login)   
-router.delete("/:productId/:reviewId", authMiddleware, deleteReview);
-
-
 
 export default router;
