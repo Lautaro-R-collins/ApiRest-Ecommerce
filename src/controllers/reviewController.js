39aea2f657e1f99d6f3713feaa718642253af3ed
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2025-12-09 18:42:44 -0300

    review controller

--- src/controllers/reviewController.js
+++ src/controllers/reviewController.js
@@ -35,29 +35,3 @@ export const createReview = async (req, res) => {
         res.status(500).json({ error: 'No se pudo crear la reseña' })
     }
 }
-
-export const deleteReview = async (req, res) => {
-    try {
-        const { productId, reviewId } = req.params
-
-        const product = await Product.findById(productId)
-        if (!product)
-            return res.status(404).json({ msg: 'Producto no encontrado' })
-
-        const review = product.reviews.id(reviewId)
-        if (!review)
-            return res.status(404).json({ msg: 'Review no encontrada' })
-
-        if (review.user.toString() !== req.user.id) {
-            return res.status(403).json({ msg: 'No autorizado' })
-        }
-
-        review.deleteOne()
-        await product.save()
-
-        res.json({ msg: 'Review eliminada' })
-    } catch (error) {
-        console.error(error)
-        res.status(500).json({ msg: 'Error del servidor' })
-    }
-}
