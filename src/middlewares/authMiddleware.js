f022e16f396a973221d7152c8ea9e958cb1c42ea
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2025-12-17 03:27:39 -0300

    add middleware

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,18 +1,23 @@
-import jwt from 'jsonwebtoken';
+import jwt from 'jsonwebtoken'
 
 export const authMiddleware = (req, res, next) => {
-    const token = req.cookies.token;
+  try {
+    const token = req.cookies.token
 
     if (!token) {
-        return res.status(401).json({ error: 'Usuario no autenticado' });
+      return res.status(401).json({ error: 'No autenticado' })
     }
 
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
-        req.user = decoded;
-        next();
-    } catch (error) {
-        return res.status(401).json({ error: 'Token inválido o expirado' });
+    // 🔥 ESTO ES LO QUE TE FALTA
+    req.user = {
+      userId: decoded.userId,
+      isAdmin: decoded.isAdmin,
     }
-};
+
+    next()
+  } catch (error) {
+    return res.status(401).json({ error: 'Token inválido' })
+  }
+}
