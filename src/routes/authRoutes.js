10897521d27b0fc1b20f20cbabd3c779d28ff727
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2025-12-17 03:27:23 -0300

    act - routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,20 +1,28 @@
 import express from 'express'
 
 import {
-    registerUser,
-    profile,
-    loginUser,
-    logoutUser,
+  registerUser,
+  profile,
+  loginUser,
+  logoutUser,
+  updateAvatar,
 } from '../controllers/authControllers.js'
+
+import { authMiddleware } from '../middlewares/authMiddleware.js'
+import { uploadAvatar } from '../middlewares/uploadAvatar.js'
 
 const router = express.Router()
 
 router.post('/register', registerUser)
-
 router.post('/login', loginUser)
-
 router.post('/logout', logoutUser)
-
 router.get('/profile', profile)
+
+router.put(
+  '/avatar',
+  authMiddleware,
+  uploadAvatar.single('avatar'),
+  updateAvatar
+)
 
 export default router
