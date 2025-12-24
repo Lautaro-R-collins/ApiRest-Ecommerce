a60cf8b8f08298daff2027de24260ba07a8524a3
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2025-12-24 11:18:22 -0300

    refactor -  middleware - auth

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,22 +1,22 @@
 import jwt from 'jsonwebtoken'
 
 export const authMiddleware = (req, res, next) => {
-  try {
-    const token = req.cookies.token
+    try {
+        const token = req.cookies.token
 
-    if (!token) {
-      return res.status(401).json({ error: 'No autenticado' })
+        if (!token) {
+            return res.status(401).json({ error: 'No autenticado' })
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+        req.user = {
+            userId: decoded.userId,
+            isAdmin: decoded.isAdmin,
+        }
+
+        next()
+    } catch (error) {
+        return res.status(401).json({ error: 'Token inválido' })
     }
-
-    const decoded = jwt.verify(token, process.env.JWT_SECRET)
-
-    req.user = {
-      userId: decoded.userId,
-      isAdmin: decoded.isAdmin,
-    }
-
-    next()
-  } catch (error) {
-    return res.status(401).json({ error: 'Token inválido' })
-  }
 }
