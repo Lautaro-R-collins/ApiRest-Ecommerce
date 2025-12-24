54c4d959064408f08369d1d9ff9a2dd7b8a8fa33
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2025-12-24 11:18:28 -0300

    refactor -  middleware - avatar

--- src/middlewares/uploadAvatar.js
+++ src/middlewares/uploadAvatar.js
@@ -2,28 +2,27 @@ import multer from 'multer'
 import path from 'path'
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads/avatars')
-  },
-  filename: (req, file, cb) => {
-    const uniqueName =
-      Date.now() + '-' + Math.round(Math.random() * 1e9)
-    cb(null, uniqueName + path.extname(file.originalname))
-  },
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/avatars')
+    },
+    filename: (req, file, cb) => {
+        const uniqueName = Date.now() + '-' + Math.round(Math.random() * 1e9)
+        cb(null, uniqueName + path.extname(file.originalname))
+    },
 })
 
 const fileFilter = (req, file, cb) => {
-  if (file.mimetype.startsWith('image/')) {
-    cb(null, true)
-  } else {
-    cb(new Error('Solo se permiten imágenes'), false)
-  }
+    if (file.mimetype.startsWith('image/')) {
+        cb(null, true)
+    } else {
+        cb(new Error('Solo se permiten imágenes'), false)
+    }
 }
 
 export const uploadAvatar = multer({
-  storage,
-  fileFilter,
-  limits: { fileSize: 2 * 1024 * 1024 }, // 2MB
+    storage,
+    fileFilter,
+    limits: { fileSize: 2 * 1024 * 1024 }, // 2MB
 })
 
 export default uploadAvatar
